ff42fbe93d99ef977afcffe7e991fa7710239bd6
santosh <santosh220903@gmail.com>
2026-01-01 22:57:35 +0530

    logical operator done

--- demo.py
+++ demo.py
@@ -23,3 +23,23 @@ print(t.find("ai"))
 a = 10
 b = 21
 print(b ** a)
+
+
+# comparison operations
+
+y = 20
+z = 33
+
+f = x == y
+f1 = x != y
+f2 = x > y
+f3 = x < y
+f4 = x >= y
+f5 = x <= y
+
+print(f)
+print(f1)
+print(f2)
+print(f3)
+print(f4)
+print(f5)
