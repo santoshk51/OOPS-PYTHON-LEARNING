88893d0c812d75f281f6057e48d4b1d944ad5ae2
santosh <santosh220903@gmail.com>
2026-01-07 22:18:06 +0530

    maps length logic done

---
+++ pract_ques/maps_length.py
@@ -0,0 +1,15 @@
+words = ["apple", "banana", "kiwi", "cherry", "mango"]
+
+count_length = {word : len(word) for word in words}
+
+print(count_length)
+
+
+#another way
+# count_length = {}
+
+for word in words:
+	count_length[word] = len(word)
+
+# print(count_length)
+	
