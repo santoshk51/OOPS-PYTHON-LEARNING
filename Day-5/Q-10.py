ce79c0f34bc474bdc4e5747825972eed269bb32b
santosh <santosh220903@gmail.com>
2026-01-12 22:17:09 +0530

    user and send message logic done

--- Day-5/Q-10.py
+++ Day-5/Q-10.py
@@ -1,7 +1,22 @@
 class User:
-	def __init__(self, username ):
+	def __init__(self, username):
 		self.username = username
 
-	def send_message(self, chatRoom, text):
-		chatRoom.send_message(self, text)
-		
+	def send_message(self, chatroom, text):
+		chatroom.send_message(self, text)
+
+	def join_chatroom(self, chatroom):
+		chatroom.add_user(self)
+	
+	def leave_chatroom(self, chatroom):
+		chatroom.remove_user(self)
+
+	
+class send_message:
+	def __init__(self, sender, text):
+		self.sender = sender
+		self.text = text
+
+	def display(self):
+		print(f"{self.sender.username}: {self.text}")
+
