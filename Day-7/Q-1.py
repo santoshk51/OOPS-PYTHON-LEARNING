ff984c2178c4330fc83cfa0d776bc7d69e71c47d
santosh <santosh220903@gmail.com>
2026-01-13 20:54:26 +0530

    write read logic done

---
+++ Day-7/Q-1.py
@@ -0,0 +1,5 @@
+# 1) Open file in write mode and write 5 names
+with open("names.txt", "w") as file:
+    for i in range(5):
+        name = input(f"Enter name {i+1}: ")
+        file.write(name + "\n")
