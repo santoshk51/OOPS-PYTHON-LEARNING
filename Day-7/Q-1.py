0fc8c1c714cf5c9d8db58bf31d0e505e2e607235
santosh <santosh220903@gmail.com>
2026-01-15 21:06:32 +0530

    read logic done

--- Day-7/Q-1.py
+++ Day-7/Q-1.py
@@ -3,7 +3,9 @@ with open("names.txt", "w") as file:
     for i in range(5):
         name = input(f"Enter name {i+1}: ")
 
-        
         file.write(name + "\n")
 
+with open("names.txt", "r") as file:
+    print("\nName in file:")
+    print(file.read())
 
