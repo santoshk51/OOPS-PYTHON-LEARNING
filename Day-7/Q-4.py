95a6f88767589771eeffbef0b85655be5c2c259b
santosh <santosh220903@gmail.com>
2026-01-16 22:09:24 +0530

    json logic processing..

---
+++ Day-7/Q-4.py
@@ -0,0 +1,17 @@
+import json
+
+with open("cities.json", "r") as f:
+	cities = json.load(f)
+
+for city, population in cities.items():
+	print(city,":",population)
+
+new_city = input("Enter new city name: ")
+new_population = int(input("Enter population: "))
+
+cities[new_city] = population
+
+with open("cities.json", "w") as file:
+	json.dump(cities, file, indent=4)
+
+print("JSON file updated successfully!")
